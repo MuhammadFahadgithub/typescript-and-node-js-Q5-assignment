4dae52c31105d6d78a6d70b615e84ac88fba25fb
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-02-28 13:43:56 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,8 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous 
+//person's name in a variable called famous_person. Then compose your
+// message and store it in a new variable called messge.Printyour 
+//message.
+let famous_person: string= "Albert Einstin";
+let quote: string= "A person who never made a mistake never tried anything new";
+let message: string= `${famous_person} once said, "${quote}"`;
+console.log(message);
